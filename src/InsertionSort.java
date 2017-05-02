b74cb01fe0473a796cf86729096166ec71420ecb
Suwadith <suwadith06@gmail.com>
2017-05-02 23:22:53 +0530

    Update InsertionSort.java

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -7,7 +7,7 @@ public class InsertionSort {
 
         int[] arr = {2, 10, 8, 6, 7, 3};
 
-        for(int a=1; a<arr.length; a++){
+        /*for(int a=1; a<arr.length; a++){
             int temp = arr[a];
             for(int b=a; b>0 && ; b--){
                 if(arr[b+1]<arr[b]){
@@ -16,7 +16,7 @@ public class InsertionSort {
                     arr[b]=temp;
                 }
             }
-        }
+        }*/
 
         for(int i=0; i<arr.length; i++){
             System.out.println(arr[i]);
